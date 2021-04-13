c146c806b6b851c741e3752cba22f2666cf6dc0d
unknown <bdelrocini4@gmail.com>
2021-04-13 11:08:34 -0400

    finished datalist singleton

--- lib/classes/erddap-data-singleton.dart
+++ lib/classes/erddap-data-singleton.dart
@@ -1,2 +1,24 @@
 import 'data-retrieval.dart';
 import 'glider-list-single.dart';
+
+class ErddapDataList{
+  static final ErddapDataList _instance = ErddapDataList._internal();
+  factory ErddapDataList() => _instance;
+
+  ErddapDataList._internal() {
+    _dataList = [];
+  }
+  
+  late List<Future<List<dynamic>>> _dataList;
+
+  List<Future<List<dynamic>>> get dataList => _dataList;
+
+  void updateList() async{
+    _dataList = [];
+    GliderList _gliderList = GliderList();
+    List<dynamic> gliderList = await _gliderList.list;
+    for(var i=0;i<gliderList.length;i++){
+      _dataList.add(Erddap.fetchData(SLAPI.getDeploymentName(gliderList[i])));
+    }
+  }
+}
